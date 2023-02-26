35765ac5836784792e241fea3da8096ebc1e1266
RoyalemPoBashke <66832472+RoyalemPoBashke@users.noreply.github.com>
2023-02-26 21:00:59 +0500

    ++

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        var scan = new Scanner(System.in);
+        System.out.println("Превет, напешы свое имя, быстра! ");
+        System.out.println("Превет " + scan.nextLine() + "!");
+        System.out.println("Тоблица синусов");
+        for (double i=0; i <= 45; i+=5)
+            {
+                System.out.println("Синус    " + i + "    " + Math.sin(i));
+            }
     }
 }
